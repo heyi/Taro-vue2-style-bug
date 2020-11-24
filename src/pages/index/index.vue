6e3523a5a412ff69c60a7733e82e54e182185168
harry <harry@fangmingyi.com>
2020-11-24 09:42:49 +0800

    feat:init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="index" :style='style'>
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data() {
+    return {
+      msg: 'Hello world!',
+      style: {
+        '-webkit-text-fill-color' : '#2233fd',
+        '-webkit-text-stroke-color' : '#445566',
+        'textAlign' :'right'
+      }
+    }
+  }
+}
+</script>
